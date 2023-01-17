54b10a3381e60219083464b2d63617c364f87a36
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-17 11:35:29 +0400

    Fix eslint error and const variable error

--- modules/Book.js
+++ modules/Book.js
@@ -1,14 +1,13 @@
-/* eslint-disable */
-
-export const formBtn = document.querySelector("#form-button");
-export const booksList = document.querySelector(".books-list");
-export const books = JSON.parse(localStorage.getItem("books")) || [];
+export const formBtn = document.querySelector('#form-button');
+export const booksList = document.querySelector('.books-list');
+export const books = JSON.parse(localStorage.getItem('books')) || [];
 
 export class Book {
   constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
   }
 
   addBook() {
@@ -16,20 +15,20 @@ export class Book {
     const bookObj = { id, title, author };
     if (books !== null) {
       books.push(bookObj);
-      localStorage.setItem("books", JSON.stringify(books));
-      document.getElementById("title").value = "";
-      document.getElementById("author").value = "";
+      localStorage.setItem('books', JSON.stringify(books));
+      document.getElementById('title').value = '';
+      document.getElementById('author').value = '';
     }
   }
 
   removeBook() {
     const { id } = this;
-    books = books.filter((book) => {
+    this.books = this.books.filter((book) => {
       if (book.id !== id) {
         return true;
       }
       return false;
     });
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
